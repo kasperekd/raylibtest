51ebdcc120e5fca344f3cdc25c588ba2046bbfd9
kasperekd <kasperekd@mail.ru>
2024-09-13 18:01:20 +0300

    clean myRay.c

--- src/visual/myRay.c
+++ src/visual/myRay.c
@@ -2,46 +2,17 @@
 
 #include "common.h"
 
-// #define MAX_CIRCLES 64
-
-// typedef struct {
-//     Vector2 position;
-//     float radius;
-//     float alpha;
-//     float speed;
-//     Color color;
-// } CircleWave;
-
 void *win_main(void *arg) {
     ThreadData *data = (ThreadData *)arg;
 
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    // SetConfigFlags(FLAG_MSAA_4X_HINT);  // NOTE: Try to enable MSAA 4X
+    // SetConfigFlags(FLAG_MSAA_4X_HINT);  // NOTE: MSAA 4X
 
     InitWindow(screenWidth, screenHeight, "raylib");
 
     InitAudioDevice();  // Initialize audio device
-
-    // Color colors[14] = {ORANGE,    RED,  GOLD,   LIME,  BLUE,    VIOLET,
-    // BROWN,
-    //                     LIGHTGRAY, PINK, YELLOW, GREEN, SKYBLUE, PURPLE,
-    //                     BEIGE};
-
-    // // Creates some circles for visual effect
-    // CircleWave circles[MAX_CIRCLES] = {0};
-
-    // for (int i = MAX_CIRCLES - 1; i >= 0; i--) {
-    //     circles[i].alpha = 0.0f;
-    //     circles[i].radius = (float)GetRandomValue(10, 40);
-    //     circles[i].position.x = (float)GetRandomValue(
-    //         (int)circles[i].radius, (int)(screenWidth - circles[i].radius));
-    //     circles[i].position.y = (float)GetRandomValue(
-    //         (int)circles[i].radius, (int)(screenHeight - circles[i].radius));
-    //     circles[i].speed = (float)GetRandomValue(1, 100) / 2000.0f;
-    //     circles[i].color = colors[GetRandomValue(0, 13)];
-    // }
 
     Music music = LoadMusicStream("resources/music.wav");
     music.looping = false;
@@ -88,35 +59,9 @@ void *win_main(void *arg) {
         // Get timePlayed
         timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music) *
                      (screenWidth - 40);
-
-        // Color circles animation
-        // for (int i = MAX_CIRCLES - 1; (i >= 0) && !pause; i--) {
-        //     circles[i].alpha += circles[i].speed;
-        //     circles[i].radius += circles[i].speed * 10.0f;
-
-        //     if (circles[i].alpha > 1.0f) circles[i].speed *= -1;
-
-        //     if (circles[i].alpha <= 0.0f) {
-        //         circles[i].alpha = 0.0f;
-        //         circles[i].radius = (float)GetRandomValue(10, 40);
-        //         circles[i].position.x = (float)GetRandomValue(
-        //             (int)circles[i].radius,
-        //             (int)(screenWidth - circles[i].radius));
-        //         circles[i].position.y = (float)GetRandomValue(
-        //             (int)circles[i].radius,
-        //             (int)(screenHeight - circles[i].radius));
-        //         circles[i].color = colors[GetRandomValue(0, 13)];
-        //         circles[i].speed = (float)GetRandomValue(1, 100) / 2000.0f;
-        //     }
-        // }
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
-        // for (int i = MAX_CIRCLES - 1; i >= 0; i--) {
-        //     DrawCircleV(circles[i].position, circles[i].radius,
-        //                 Fade(circles[i].color, circles[i].alpha));
-        // }
 
         DrawRectangle(20, screenHeight - 20 - 12, screenWidth - 40, 12,
                       LIGHTGRAY);
