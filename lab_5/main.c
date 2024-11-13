f5868a08ba518f86262e0be4f962e9e1e6dc569a
kasperekd <kasperekd@mail.ru>
2024-11-13 21:27:25 +0700

    added hardware crc

--- lab_5/main.c
+++ lab_5/main.c
@@ -1,3 +1,4 @@
+#include <nmmintrin.h>  // sse4.1 для CRC32
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -10,7 +11,6 @@ uint8_t calculate_crc(uint8_t data[], int length, uint8_t poly) {
     uint8_t crc = 0;
     for (int i = 0; i < length; i++) {
         crc ^= data[i];
-        // crc ^= (data[i] << (8 - 1));
         for (int j = 0; j < 8; j++) {
             if (crc & 0x80) {
                 crc = (crc << 1) ^ poly;
@@ -20,6 +20,14 @@ uint8_t calculate_crc(uint8_t data[], int length, uint8_t poly) {
         }
     }
     return crc;
+}
+
+uint32_t calculate_crc32_hardware(uint8_t data[], int length) {
+    uint32_t crc = 0xFFFFFFFF;  // Начальное значение
+    for (int i = 0; i < length; i++) {
+        crc = _mm_crc32_u8(crc, data[i]);
+    }
+    return crc ^ 0xFFFFFFFF;  // Инверсия
 }
 
 bool check_crc(uint8_t data[], int length, uint8_t poly) {
@@ -43,19 +51,24 @@ void print_bits(const uint8_t *data, size_t length) {
 int main() {
     uint8_t data[DATA_BITS / 8] = {0};
     for (int i = 0; i < DATA_BITS / 8; i++) {
-        // data[i] = (uint8_t)(i % 256);
         data[i] = i % 2 ? 0xFF : 0x00;
     }
+
     print_bits(data, sizeof(data));
     printf("\n");
-    uint8_t crc = calculate_crc(data, DATA_BITS / 8, POLY);
-    printf("Calculated CRC: 0x%02X\n", crc);
+
+    uint8_t crc8 = calculate_crc(data, DATA_BITS / 8, POLY);
+    printf("Calculated CRC-8: 0x%02X\n", crc8);
+
+    uint32_t crc32_hw = calculate_crc32_hardware(data, DATA_BITS / 8);
+    printf("Calculated CRC-32 (hardware): 0x%08X\n", crc32_hw);
 
     uint8_t transmitted_data[(DATA_BITS / 8) + 1];
     for (int i = 0; i < DATA_BITS / 8; i++) {
         transmitted_data[i] = data[i];
     }
-    transmitted_data[DATA_BITS / 8] = crc;
+    transmitted_data[DATA_BITS / 8] = crc8;
+
     if (check_crc(transmitted_data, (DATA_BITS / 8) + 1, POLY)) {
         printf("Transmitted packet accepted without errors.\n");
     } else {
@@ -66,7 +79,8 @@ int main() {
     printf("\n");
 
     int detected_errors = 0, undetected_errors = 0;
-    for (int bit = 0; bit < DATA_BITS + POLY_BITS - 1; bit++) {
+    // for (int bit = 0; bit < DATA_BITS + POLY_BITS - 2; bit++) {
+    for (int bit = 0; bit < DATA_BITS; bit++) {
         flip_bit(transmitted_data, bit);
 
         if (check_crc(transmitted_data, (DATA_BITS / 8) + 1, POLY)) {
@@ -75,7 +89,7 @@ int main() {
             detected_errors++;
         }
 
-        // flip_bit(transmitted_data, bit);
+        flip_bit(transmitted_data, bit);
     }
 
     printf("Detected errors: %d\n", detected_errors);
